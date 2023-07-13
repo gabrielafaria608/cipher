80192f6e178e352db919650b860befca752356fe
Gabriela Faria <gabriela.faria806@gmail.com>
2023-07-13 16:01:31 -0300

    T001: feat - Criação da estrutura básica de HTML

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let btnEncrypt = document.getElementById("btnShowEncrypt");
+let btnDecrypt = document.getElementById("btnShowDecrypt");
+
+btnEncrypt.addEventListener("click", showEncrypt);
+btnDecrypt.addEventListener("click", showDecrypt);
+
+function showEncrypt(){
+    let contentEncrypt = document.querySelector(".encrypt");
+    if(contentEncrypt.style.display === "none"){
+        contentEncrypt.style.display = "block";
+    }
+    else{
+        contentEncrypt.style.display = "none";
+    }
+}
+function showDecrypt(){
+    let contentDecrypt = document.querySelector(".decrypt")
+    if(contentDecrypt.style.display === "none"){
+        contentDecrypt.style.display = "block";
+    }
+    else{
+        contentDecrypt.style.display = "none";
+    }
+}
